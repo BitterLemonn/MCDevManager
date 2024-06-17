3a8c299f10834ea31420c00bd2521daef2df7f74
bitterlemon <873811906@qq.com>
2024-06-17 18:11:28 +0800

    first commit

--- app/src/main/java/com/lemon/mcdevmanager/data/common/ConstantValue.kt
+++ app/src/main/java/com/lemon/mcdevmanager/data/common/ConstantValue.kt
@@ -3,3 +3,6 @@ package com.lemon.mcdevmanager.data.common
 // navigation 地址
 const val SPLASH_PAGE = "SPLASH_PAGE"
 const val LOGIN_PAGE = "LOGIN_PAGE"
+
+// sm4 加密公钥
+const val SM4_PUBLIC_KEY = "BC60B8B9E4FFEFFA219E5AD77F11F9E2"
