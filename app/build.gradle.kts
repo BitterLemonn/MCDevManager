7cedfd0f2a3f08332c032d42b8d71de9360699e7
bitterlemon <873811906@qq.com>
2024-11-12 14:24:28 +0800

    [release] v0.5.0 添加收益详情 收益结算

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lemon.mcdevmanager"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.4.2"
+        versionCode = 9
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
