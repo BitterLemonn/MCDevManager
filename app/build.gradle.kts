adf4cb2b5a5faab1455aac8ee73b4318c262d755
bitterlemon <873811906@qq.com>
2025-07-08 23:15:01 +0800

    回退网易登录接口更新 添加cookies登录检查

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lemon.mcdevmanager"
         minSdk = 26
         targetSdk = 34
-        versionCode = 17
-        versionName = "0.6.3"
+        versionCode = 18
+        versionName = "0.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
